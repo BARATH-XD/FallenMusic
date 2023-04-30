d32748f26a74b871daeb9339970d2f53140f4333
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-04-30 12:51:43 +0530

    Update config.py
    Signed-off-by: BARATH <115382856+ITZ-BARATH@users.noreply.github.com>

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/b37ac825e9be7086d5d00.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/253f6d66c0544a1833ae0.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/cring_tamilchatting")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/cring_tamilchatting")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
